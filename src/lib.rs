961a9537002226f2aae7c484324f0e7211007377
Runji Wang <wangrunji0408@163.com>
2020-04-07 02:03:13 +0800

    store context on the stack

--- src/lib.rs
+++ src/lib.rs
@@ -24,14 +24,10 @@ pub union ThreadFuture<F, T> {
 /// So that we can quickly locate it from stack pointer (just like Linux).
 #[repr(C)]
 struct TCB<F, T> {
-    /// Pointer to the executor context.
+    /// Pointer to the context of executor or thread.
     ///
-    /// This value is set by the executor before running a thread.
     /// Running thread call `switch` on this to switch back to executor.
-    executor_context_ptr: usize,
-
-    /// The register context of thread.
-    context: ThreadContext,
+    context_ptr: *mut ThreadContext,
 
     /// A canary value to detect stack overflow.
     canary: usize,
@@ -39,6 +35,8 @@ struct TCB<F, T> {
     /// Thread state. Contains function object or return value.
     state: State<F, T>,
 }
+
+unsafe impl<F, T> Send for TCB<F, T> {}
 
 const RAW_SIZE: usize = 0x2000;
 
@@ -81,37 +79,41 @@ impl<F, T> State<F, T> {
 #[repr(C)]
 #[derive(Default, Debug)]
 struct ThreadContext {
-    rsp: usize,
     rbx: usize,
     rbp: usize,
     r12: usize,
     r13: usize,
     r14: usize,
     r15: usize,
+    rip: usize,
 }
 
 impl ThreadContext {
     /// Switch context to another thread.
     #[naked]
     #[inline(never)]
-    unsafe extern "sysv64" fn switch(&mut self, _target: &mut Self) {
+    unsafe extern "sysv64" fn switch(_ptr_ptr: *mut *mut Self) {
         asm!(r#"
-        mov [rdi], rsp
-        mov [rdi + 8*1], rbx
-        mov [rdi + 8*2], rbp
-        mov [rdi + 8*3], r12
-        mov [rdi + 8*4], r13
-        mov [rdi + 8*5], r14
-        mov [rdi + 8*6], r15
+        // push rip (by caller)
+        push r15
+        push r14
+        push r13
+        push r12
+        push rbp
+        push rbx
 
-        mov rbx, [rsi + 8*1]
-        mov rbp, [rsi + 8*2]
-        mov r12, [rsi + 8*3]
-        mov r13, [rsi + 8*4]
-        mov r14, [rsi + 8*5]
-        mov r15, [rsi + 8*6]
-        mov rsp, [rsi]
-        "# :::: "volatile" "intel");
+        mov rax, [rdi]
+        mov [rdi], rsp
+        mov rsp, rax
+
+        pop rbx
+        pop rbp
+        pop r12
+        pop r13
+        pop r14
+        pop r15
+        // pop rip (by ret)
+        "# :::: "volatile" "intel" "alignstack");
     }
 }
 
@@ -128,8 +130,7 @@ where
         assert_eq!(core::mem::size_of::<Self>(), RAW_SIZE, "TCB size exceed");
         ThreadFuture {
             tcb: ManuallyDrop::new(TCB {
-                executor_context_ptr: 0,
-                context: Default::default(),
+                context_ptr: core::ptr::null_mut(),
                 state: State::Ready(f),
                 canary: CANARY,
             }),
@@ -146,19 +147,16 @@ where
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         // allocate executor context at stack
-        let mut context = ThreadContext::default();
         let raw = self.get_mut();
         let state = unsafe {
-            // fill 'rsp' at first run
+            // fill SP and PC at first run
             if let State::Ready(_) = &raw.tcb.state {
-                let rsp = &mut raw.stack[RAW_SIZE / 8 - 1];
-                *rsp = entry::<F, T> as usize;
-                raw.tcb.context.rsp = rsp as *mut _ as usize;
+                let context = ((raw as *mut Self).add(1) as *mut ThreadContext).sub(1);
+                (*context).rip = entry::<F, T> as usize;
+                raw.tcb.context_ptr = context;
             }
-            // fill pointer to my context so that the thread can switch back
-            raw.tcb.executor_context_ptr = &mut context as *mut _ as usize;
             // switch to the thread
-            context.switch(&mut raw.tcb.context);
+            ThreadContext::switch(&mut raw.tcb.context_ptr);
             &mut raw.tcb.state
         };
         // check the thread state
@@ -205,8 +203,7 @@ pub fn yield_now() {
             "canary is changed. maybe stack overflow!"
         );
         // switch back to the executor thread
-        let executor_context = &mut *(tcb.executor_context_ptr as *mut _);
-        tcb.context.switch(executor_context);
+        ThreadContext::switch(&mut tcb.context_ptr);
     }
 }
 
